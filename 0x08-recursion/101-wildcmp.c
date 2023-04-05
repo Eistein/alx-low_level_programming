ddb880827bb56c0d02a6d397a151cb74523ed909
Eistein <93629760+Eistein@users.noreply.github.com>
2023-04-05 21:55:30 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,22 +9,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	while (*s1)
+	if (*s1 == '\0')
 	{
-		if (*s2 == '*')
+		if (*s2 != '\0' && *s2 == '*')
 		{
-			return (wildcmp(s1++, s2) || wildcmp(s1, s2++));
+			return (wildcmp(s1, s2++));
 		}
-		else if (*s1++ != *s2++)
-		{
-			return (0);
-		}
+		return (*s2 == '\0');
 	}
 
-	while (*s2 == '*')
+	if (*s2 == '*')
 	{
-		s2++;
+		return (wildcmp(s1++, s2) || wildcmp(s1, s2++));
 	}
-
-	return (*s1 == *s2);
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1++, s2++));
+	}
+	return (0);
 }
