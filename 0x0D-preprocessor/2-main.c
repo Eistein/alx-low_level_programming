db13a2f1bbdfac6937b6e4fa930e8d85cc024d2e
Eistein <didganza7@gmail.com>
2023-04-17 10:33:05 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of compiled file
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
