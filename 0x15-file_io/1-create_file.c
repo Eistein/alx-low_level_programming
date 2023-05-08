049bf73238e029bc9d61a442805e528bdb3d3105
Eistein <didganza7@gmail.com>
2023-05-08 11:23:31 -0700

    task1,2,3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,22 +10,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-	return (-1);
+		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-	return (-1);
-
-	if (text_content != NULL) {
-	while (text_content[bytes_written])
-	bytes_written++;
-	bytes_written = write(fd, text_content, bytes_written);
-	if (bytes_written == -1)
-	return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
 
 	close(fd);
 
